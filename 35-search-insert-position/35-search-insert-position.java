f248052273477ebda44f09d82ca14dfc8df40d01
AKASHCHAURASIA2212 <102942376+AKASHCHAURASIA2212@users.noreply.github.com>
2022-05-27 19:23:51 +0530

    Time: 11 ms (9.01%), Space: 44 MB (17.68%) - LeetHub

--- 35-search-insert-position/35-search-insert-position.java
+++ 35-search-insert-position/35-search-insert-position.java
@@ -4,16 +4,13 @@ class Solution {
     {
          int i = 0 ;
          int j = nums.length-1;
-         
-        int premid = 0;
         int mid = 0;
         while(i<=j)
         {
             
             mid = (i+j)/2;
-            premid = mid;
             System.out.println("mid-->"+mid);
-            System.out.println("premid-->"+premid);
+            // System.out.println("premid-->"+premid);
             
             if(nums[mid]==target)
             {
@@ -29,28 +26,10 @@ class Solution {
             }
         }
         
-        return premid;
+        return mid;
     }
     public int searchInsert(int[] nums, int target) {
         int idx = BinSearch(nums,target);
-//         // System.out.println("temp1-->"+temp1);
-//         if(temp1==-1)
-//         {
-//           int temp2 = BinSearch(nums,target-1);
-//            // System.out.println("temp2-->"+temp2);
-//              if(temp2!=-1)
-//             {
-//                 return temp2+1;
-//             }
-            
-//           int temp3 = BinSearch(nums,target+1); 
-//         // System.out.println("temp3-->"+temp3);
-
-//             if(temp3!=-1)
-//             {
-//                   return temp3;
-//             }
-//         }
         if(nums[idx]==target)
         {
             return idx;
