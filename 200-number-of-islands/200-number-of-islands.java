3b08f87f8d034f2e6f43071e05c081294474880b
AKASHCHAURASIA2212 <102942376+AKASHCHAURASIA2212@users.noreply.github.com>
2022-06-03 00:56:46 +0530

    Time: 6 ms (37.56%), Space: 52 MB (73.00%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.java
+++ 200-number-of-islands/200-number-of-islands.java
@@ -2,11 +2,8 @@ class Solution {
     
     public void jump(int i,int j,char[][] grid)
     {
+        grid[i][j]='0';
         
-        if(i<0 || j<0 || i>=grid.length || j>=grid[0].length)
-        {
-            return;
-        }
        int[][] dir = {{-1,0},{0,-1},{1,0},{0,1}};
         for(int k = 0 ; k<dir.length ;k++)
         {
@@ -14,15 +11,11 @@ class Solution {
             int row = i  +  dir[k][0];
             int col = j  +  dir[k][1];
             
-            if(row<0 || col<0 || row>=grid.length || col>=grid[0].length)
+            if(row>=0 && col>=0 && row<grid.length && col<grid[0].length && grid[row][col]=='1')
             {
-                continue;
+               jump(row,col,grid);
             }
-            if(grid[row][col]=='1')
-              {
-              grid[row][col]='0';
-                jump(row,col,grid);
-              }
+        
         }
      
         
@@ -35,7 +28,6 @@ class Solution {
            {
                if(grid[i][j]=='1')
                {
-                   grid[i][j]='0';
                   count++;
                    jump(i,j,grid);
                }
