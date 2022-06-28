f5fea5e9a35dd5d4ed07828ebe2180dd8772bb04
AKASHCHAURASIA2212 <102942376+AKASHCHAURASIA2212@users.noreply.github.com>
2022-06-28 22:39:51 +0530

    Time: 3 ms (26.88%), Space: 54.2 MB (68.16%) - LeetHub

---
+++ 1034-coloring-a-border/1034-coloring-a-border.java
@@ -0,0 +1,112 @@
+class Solution {
+    
+    // public boolean treverse(int i,int j,int orgclr,int[][] grid) {
+    //     if(isBorder(grid,i+1,j,orgclr))
+    //     {
+    //         return true;
+    //     }
+    //     else if(isBorder(grid,i-1,j,orgclr))
+    //     {
+    //         return true;
+    //     }
+    //     else if(isBorder(grid,i,j+1,orgclr))
+    //     {
+    //         return true;
+    //     }
+    //     else if(isBorder(grid,i,j-1,orgclr))
+    //     {
+    //         return true;
+    //     }
+    //     else{
+    //       return false;
+    //     }
+// }
+    
+    public boolean isBorder(int i,int j,int orgclr,int[][] grid) {
+      if(i==0 || j==0 || i==grid.length-1 || j==grid[0].length-1) {
+             return true;
+        }
+        else if(grid[i+1][j]!=orgclr)
+        {
+            return true;
+        }
+        else if(grid[i-1][j]!=orgclr)
+        {
+            return true;
+        }
+        else if(grid[i][j+1]!=orgclr)
+        {
+            return true;
+        }
+        else if(grid[i][j-1]!=orgclr)
+        {
+            return true;
+        }
+        
+        return false;
+}
+    
+    public class Pair{
+        int i;
+        int j;
+        boolean border;
+        public Pair(int i,int j,boolean border) {
+            this.i=i;
+            this.j=j;
+            this.border=border;
+        }
+    }
+    
+    public void addNB(int i,int j,int orgclr,Queue<Pair> qu ,int[][] grid,boolean[][] vis) {
+       if(i<0 || j<0 || i>=grid.length || j>=grid[0].length ||vis[i][j]==true || grid[i][j]!=orgclr) {
+           return;
+       }
+        
+        qu.add(new Pair(i,j,false));
+    }
+    public int[][] colorBorder(int[][] grid, int row, int col, int color) {
+        int orgclr = grid[row][col];
+        
+        Queue<Pair> qu = new LinkedList<>();
+        boolean[][] vis = new boolean[grid.length][grid[0].length];
+        qu.add(new Pair(row,col,false));
+        
+        ArrayList<Pair> al = new ArrayList<>();
+        
+        while(qu.size()>0)
+        {
+           Pair rem = qu.remove();
+            
+            if(vis[rem.i][rem.j])
+            {
+                continue;
+            }
+            vis[rem.i][rem.j]=true;
+            
+            boolean border = isBorder(rem.i,rem.j,orgclr,grid);
+            rem.border = border;
+            
+            if(border)
+            {
+               al.add(rem);  
+            }
+           
+            
+            addNB(rem.i+1,rem.j,orgclr,qu,grid,vis);
+            addNB(rem.i-1,rem.j,orgclr,qu,grid,vis);
+            addNB(rem.i,rem.j+1,orgclr,qu,grid,vis);
+            addNB(rem.i,rem.j-1,orgclr,qu,grid,vis);
+         
+        }
+        
+        for(Pair ele : al)
+        {
+            if(ele.border)
+            {
+                grid[ele.i][ele.j]=color;
+            }
+        }
+        
+        return grid;
+    }
+}
