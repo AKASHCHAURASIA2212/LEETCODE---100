0c4fd49b74254c34ed9f42ad9cb1978d5e628fb9
AKASHCHAURASIA2212 <102942376+AKASHCHAURASIA2212@users.noreply.github.com>
2022-06-29 22:26:44 +0530

    Time: 14 ms (28.32%), Space: 62.3 MB (47.39%) - LeetHub

--- 463-island-perimeter/463-island-perimeter.java
+++ 463-island-perimeter/463-island-perimeter.java
@@ -2,10 +2,10 @@
 
 class Solution {
     
-    public void treverse(int[][] grid,int i,int j,boolean[][] vis)
+    public void treverse(int[][] grid,int i,int j)
     {
       
-        if(i<0 || j<0 || i>=grid.length || j>=grid[0].length || vis[i][j]==true || grid[i][j]==0)
+        if(i<0 || j<0 || i>=grid.length || j>=grid[0].length || grid[i][j]==0)
         {
             maxperimter++;
             return;
@@ -15,25 +15,24 @@ class Solution {
         {
           return;
         }
-        vis[i][j]=true;
     }
     
     static int maxperimter = 0 ;
     public int islandPerimeter(int[][] grid) {
          
-        boolean[][] vis = new boolean[grid.length][grid[0].length];
+        // boolean[][] vis = new boolean[grid.length][grid[0].length];
         
         int mymax=0;
         for(int i=0 ; i< grid.length ; i++)
         {
             for(int j=0 ; j< grid[0].length ; j++)
             {
-               if(grid[i][j]==1 && vis[i][j]==false)
+               if(grid[i][j]==1)
                {
-                     treverse(grid,i-1,j,vis);
-                     treverse(grid,i+1,j,vis);
-                     treverse(grid,i,j-1,vis);
-                     treverse(grid,i,j+1,vis);
+                     treverse(grid,i-1,j);
+                     treverse(grid,i+1,j);
+                     treverse(grid,i,j-1);
+                     treverse(grid,i,j+1);
                    
                    mymax +=maxperimter;
                    maxperimter=0;
