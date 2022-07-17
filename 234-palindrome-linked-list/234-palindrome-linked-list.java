c6bb89b24f22b56ae869e51b1741df6909bae438
AKASHCHAURASIA2212 <102942376+AKASHCHAURASIA2212@users.noreply.github.com>
2022-07-17 17:39:50 +0530

    Time: 7 ms (65.58%), Space: 97.2 MB (64.61%) - LeetHub

--- 234-palindrome-linked-list/234-palindrome-linked-list.java
+++ 234-palindrome-linked-list/234-palindrome-linked-list.java
@@ -9,66 +9,36 @@
  * }
  */
 class Solution {
-    
-    public ListNode Reverse(ListNode head)
-    {
-        if(head.next==null)
-        {
-            return head ;
-        }
-        ListNode prev = null ,curr = head , next = head.next ;
-        
-        while(next!=null)
-        {
-            next = curr.next ; 
-            curr.next = prev ;
-            prev = curr ;
-            curr = next ;
-        }
-        return prev ;
-    }
     public boolean isPalindrome(ListNode head) {
-        
-        ListNode slow =head ,fast = head ;
-        
-        while(fast!=null && fast.next!=null)
-        {   System.out.println("slow-->"+slow.val);
-           slow = slow.next ; 
-         System.out.println("slow-->"+slow.val);
-            fast = fast.next.next ;
-        }
-        
-        ListNode nhead = Reverse(slow);
-        
-        // System.out.println("hval-->"+head.val+" nhval-->"+nhead.val);
-
-        while( head!=null && nhead!=null)
-        {
-            System.out.println("hval-->"+head.val+" nhval-->"+nhead.val);
-            if(head.val!=nhead.val)
-            {
-                return false ; 
-            }
-            head = head.next ;
-            nhead = nhead.next ;
-        }
-        
-        if(head!=null)
-        {
-          if(head.next!=null)
-          {
-              return false;
-          }
-        }
-        
-        if(nhead!=null)
-        {
-          if(nhead.next!=null)
-          {
-              return false;
-          }
-        }
-        
-        return true ; 
+    ListNode fast = head, slow = head;
+    while (fast != null && fast.next != null) {
+        fast = fast.next.next;
+        slow = slow.next;
     }
+    if (fast != null) { // odd nodes: let right half smaller
+        slow = slow.next;
+    }
+    slow = reverse(slow);
+    fast = head;
+    
+    while (slow != null) {
+        if (fast.val != slow.val) {
+            return false;
+        }
+        fast = fast.next;
+        slow = slow.next;
+    }
+    return true;
+}
+
+public ListNode reverse(ListNode head) {
+    ListNode prev = null;
+    while (head != null) {
+        ListNode next = head.next;
+        head.next = prev;
+        prev = head;
+        head = next;
+    }
+    return prev;
+}
 }
