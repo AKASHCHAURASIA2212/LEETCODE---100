fd9ed814f0bdb6abd608c406e7acc92b3857239b
AKASHCHAURASIA2212 <102942376+AKASHCHAURASIA2212@users.noreply.github.com>
2022-07-22 14:08:33 +0530

    Time: 1 ms (100.00%), Space: 56.5 MB (34.23%) - LeetHub

--- 169-majority-element/169-majority-element.java
+++ 169-majority-element/169-majority-element.java
@@ -1,28 +1,50 @@
 class Solution {
     public int majorityElement(int[] nums) {
         // brute force 
-        if(nums.length==1)
-        {
-            return nums[0];
-        }
-        HashMap<Integer,Integer> hm = new HashMap<>();
-        int n = nums.length/2 ;
-        for(int i=0;i<nums.length;i++)
-        {
-            if(hm.containsKey(nums[i]))
-            {
-                hm.put(nums[i],hm.get(nums[i])+1);
+//         if(nums.length==1)
+//         {
+//             return nums[0];
+//         }
+//         HashMap<Integer,Integer> hm = new HashMap<>();
+//         int n = nums.length/2 ;
+//         for(int i=0;i<nums.length;i++)
+//         {
+//             if(hm.containsKey(nums[i]))
+//             {
+//                 hm.put(nums[i],hm.get(nums[i])+1);
                 
-                if(hm.get(nums[i])>n)
-                {
-                    return nums[i];
-                }
+//                 if(hm.get(nums[i])>n)
+//                 {
+//                     return nums[i];
+//                 }
+//             }
+//             else
+//             {
+//                 hm.put(nums[i],1);
+//             }
+//         }
+//         return -1;
+        
+        int c = 1 ; 
+        int me = nums[0];
+        
+        for(int i=1;i<nums.length;i++)
+        {
+            if(nums[i]==me)
+            {
+                c++;
             }
             else
             {
-                hm.put(nums[i],1);
+                --c ; 
+                if(c==0)
+                {
+                    me = nums[i];
+                    c=1 ;
+                }
             }
         }
-        return -1;
+        
+        return me ; 
     }
 }
