9b74e96783b0705638e6f5c63d5dd33c3f9c4e33
AKASHCHAURASIA2212 <102942376+AKASHCHAURASIA2212@users.noreply.github.com>
2022-07-25 20:42:18 +0530

    Time: 1 ms (100.00%), Space: 42 MB (7.23%) - LeetHub

--- 231-power-of-two/231-power-of-two.java
+++ 231-power-of-two/231-power-of-two.java
@@ -1,28 +1,32 @@
 class Solution {
     
-    public boolean isPower(int p,int n)
-    {
-        int mynum =(int) Math.pow(2,p);
+//     public boolean isPower(int p,int n)
+//     {
+//         int mynum =(int) Math.pow(2,p);
         
-        if(mynum==n)
-        {
-            return true ; 
-        }
-        else if(mynum>n)
-        {
-            return false ;
-        }
-        else
-        {
-            return isPower(p+1,n);
-        }
-    }
-    public boolean isPowerOfTwo(int n) {
+//         if(mynum==n)
+//         {
+//             return true ; 
+//         }
+//         else if(mynum>n)
+//         {
+//             return false ;
+//         }
+//         else
+//         {
+//             return isPower(p+1,n);
+//         }
+//     }
+//     public boolean isPowerOfTwo(int n) {
         
-        if(n==Integer.MAX_VALUE)
-        {
-            return false ;
-        }
-        return isPower(0,n);
+//         if(n==Integer.MAX_VALUE)
+//         {
+//             return false ;
+//         }
+//         return isPower(0,n);
+//     }
+    
+      public boolean isPowerOfTwo(int n) {
+        return n > 0 && (n & n - 1) == 0;
     }
 }
