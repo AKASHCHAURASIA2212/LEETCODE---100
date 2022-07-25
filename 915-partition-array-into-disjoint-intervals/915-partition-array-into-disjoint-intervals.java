b8c68f47cc14df4093238a38c621bd5c9a63f900
AKASHCHAURASIA2212 <102942376+AKASHCHAURASIA2212@users.noreply.github.com>
2022-07-25 22:47:17 +0530

    Time: 2 ms (100.00%), Space: 88.1 MB (65.14%) - LeetHub

--- 915-partition-array-into-disjoint-intervals/915-partition-array-into-disjoint-intervals.java
+++ 915-partition-array-into-disjoint-intervals/915-partition-array-into-disjoint-intervals.java
@@ -1,36 +1,62 @@
 class Solution {
+//     public int partitionDisjoint(int[] nums) {
+        
+//         int min[] = new int[nums.length];
+        
+//         int max[] = new int [nums.length];
+        
+//         int n = nums.length;
+        
+//         min[n-1]=nums[n-1];
+            
+//         for(int i = n-2 ; i>=0 ;i--)
+//         {
+//             min[i] = Math.min(nums[i],min[i+1]);
+//         }
+        
+//         max[0]=nums[0];
+            
+//         for(int i =1; i<n ;i++)
+//         {
+//             max[i] = Math.max(nums[i],max[i-1]);
+//         }
+        
+        
+//         for(int i=0;i<n-1;i++)
+//         {
+            
+//             if(max[i]<=min[i+1] && nums[i]<=max[i])
+//             {
+//                 return i+1;
+//             }
+//         }
+        
+//         return -1 ;
+//     }
+    
     public int partitionDisjoint(int[] nums) {
         
-        int min[] = new int[nums.length];
+        int mtp=nums[0];
         
-        int max[] = new int [nums.length];
+        int mts=nums[0];
         
-        int n = nums.length;
+        int idx=0;
         
-        min[n-1]=nums[n-1];
+        for(int i=1;i<nums.length;i++){
             
-        for(int i = n-2 ; i>=0 ;i--)
-        {
-            min[i] = Math.min(nums[i],min[i+1]);
-        }
-        
-        max[0]=nums[0];
-            
-        for(int i =1; i<n ;i++)
-        {
-            max[i] = Math.max(nums[i],max[i-1]);
-        }
-        
-        
-        for(int i=0;i<n-1;i++)
-        {
-            
-            if(max[i]<=min[i+1] && nums[i]<=max[i])
-            {
-                return i+1;
+            if(nums[i]<mtp){
+                
+                idx=i;
+                
+                mtp=mts;
+                
+            }else{
+                
+                mts=Math.max(mts,nums[i]);
+                
             }
         }
-        
-        return -1 ;
+        return idx+1;
     }
+
 }
