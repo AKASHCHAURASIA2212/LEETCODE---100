84907bdb91a4b3dfed29c4911ca0f23f6269e9f3
AKASHCHAURASIA2212 <102942376+AKASHCHAURASIA2212@users.noreply.github.com>
2022-07-25 20:56:56 +0530

    Time: 25 ms (34.33%), Space: 47.8 MB (14.39%) - LeetHub

--- 326-power-of-three/326-power-of-three.java
+++ 326-power-of-three/326-power-of-three.java
@@ -1,29 +1,38 @@
 class Solution {
 
-    public boolean isPower(int p,int n)
-    {
-        int mynum =(int) Math.pow(3,p);
+//     public boolean isPower(int p,int n)
+//     {
+//         int mynum =(int) Math.pow(3,p);
         
-        if(mynum==n)
-        {
-            return true ; 
-        }
-        else if(mynum>n)
-        {
-            return false ;
-        }
-        else
-        {
-            return isPower(p+1,n);
-        }
-    }
-    public boolean isPowerOfThree(int n) {
+//         if(mynum==n)
+//         {
+//             return true ; 
+//         }
+//         else if(mynum>n)
+//         {
+//             return false ;
+//         }
+//         else
+//         {
+//             return isPower(p+1,n);
+//         }
+//     }
+//     public boolean isPowerOfThree(int n) {
         
-        if(n==Integer.MAX_VALUE)
-        {
-            return false ;
-        }
-        return isPower(0,n);
+//         if(n==Integer.MAX_VALUE)
+//         {
+//             return false ;
+//         }
+//         return isPower(0,n);
+//     }
+   public boolean isPowerOfThree(int n) 
+   {
+	  if(n <= 1) 
+      {
+          return n == 1;
+      }
+       
+	   return n % 3 == 0 && isPowerOfThree(n / 3);
     }
     
-    }
+ }
