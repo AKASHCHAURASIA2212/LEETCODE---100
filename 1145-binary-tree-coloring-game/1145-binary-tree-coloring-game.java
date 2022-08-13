4f28372d9b5a61ab36e6c362d6a12ac6b7f6afa0
AKASHCHAURASIA2212 <102942376+AKASHCHAURASIA2212@users.noreply.github.com>
2022-08-13 23:02:44 +0530

    Time: 0 ms (100.00%), Space: 39.9 MB (96.83%) - LeetHub

--- 1145-binary-tree-coloring-game/1145-binary-tree-coloring-game.java
+++ 1145-binary-tree-coloring-game/1145-binary-tree-coloring-game.java
@@ -1,83 +1,36 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
-class Solution {
-    
-    int size(TreeNode root)
-    {
-        if(root==null)
-        {
-            return 0 ;
+    class Solution {
+
+        int xkaleft;
+        int xkaright;
+
+        public int size(TreeNode node, int x){
+            if(node == null){
+                return 0;
+            }
+
+            int ls = size(node.left, x);
+            int rs = size(node.right, x);
+
+            if(node.val == x){
+                xkaleft = ls;
+                xkaright = rs;
+            }
+
+            int ts = ls + rs + 1;
+            return ts;
         }
-        
-        int ls = size(root.left);
-        int rs = size(root.right);
-        
-        int mysize = ls+rs+1;
-        
-        return mysize;
+
+        public boolean btreeGameWinningMove(TreeNode root, int n, int x) {
+            size(root, x);
+
+            int theOtherSide = n - (xkaleft + xkaright + 1);
+            int maxOfThree = Math.max(theOtherSide, Math.max(xkaleft, xkaright));
+
+            int rest = n - maxOfThree;
+            if(maxOfThree > rest){
+                return true;
+            } else {
+                return false;
+            }
+        }
     }
-    
-    TreeNode findNode(TreeNode root,int x)
-    {
-        if(root==null)
-        {
-           return null ;
-        }
-        if(root.val==x)
-        {
-            return root ;
-        }
-       TreeNode n1 =  findNode(root.left,x);
-        
-        if(n1!=null)
-        {
-            return n1 ;
-        }
-       TreeNode n2 = findNode(root.right,x);
-         if(n2!=null)
-        {
-            return n2 ;
-        }
-        
-        return null ;
-        
-    }
-    public boolean btreeGameWinningMove(TreeNode root, int n, int x) {
-        
-        TreeNode p1root = findNode(root,x);
-        
-        int s1 = size(p1root.left);
-        int s2 = size(p1root.right);
-        int s3 = n - (s1+s2+1);
-        
-        int max = Math.max(s1,Math.max(s2,s3));
-        
-        System.out.println("s1--> "+s1);
-        System.out.println("s2--> "+s2);
-        System.out.println("s3--> "+s3);
-        
-        
-        if(n-max<=n/2)
-        {
-            return true;
-        }
-        
-        return false ;
-     
-        
-        
-    }
-}
