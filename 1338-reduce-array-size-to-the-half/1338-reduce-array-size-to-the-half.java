e86305c2c356c3da320ad4b025c52394aea9dfa3
AKASHCHAURASIA2212 <102942376+AKASHCHAURASIA2212@users.noreply.github.com>
2022-08-18 19:07:26 +0530

    Time: 128 ms (26.12%), Space: 86.3 MB (57.26%) - LeetHub

--- 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.java
+++ 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.java
@@ -1,45 +1,25 @@
 class Solution {
-    
-    public void Display(int[] arr)
-    {
-       for(int i=0;i<arr.length;i++)
-       {
-           System.out.print(arr[i]+ " ");
-       }
-    }
     public int minSetSize(int[] arr) {
-        HashMap<Integer,Integer> hm = new HashMap<>();    
-        for(int i=0;i<arr.length;i++) {
-            int val = arr[i];   
-            if(hm.containsKey(val))  {
-                hm.put(val, hm.get(val) + 1);   
-            } else
-            {
-                hm.put(val,1);
-            }
+        int half = arr.length / 2;
+        int result = 0;
+        int fi = 0;
+        HashMap<Integer, Integer> hm = new HashMap<>();
+        for(int num: arr){
+            hm.put(num, hm.getOrDefault(num,0)+1);
         }
-          System.out.println(hm.keySet());
-     
-        int size[] = new int[hm.size()];
-       int  j=0;
-        for(int key : hm.keySet())
-        {
-            size[j]=hm.get(key);
-            j++;
-        }
- 
-        Arrays.sort(size);
-        int occr=0,count=0;
-        for(int i=size.length-1;i>=0;i--)
-        {
-            occr+=size[i];
-            count++;
-            if(occr>=arr.length/2)
-            {
-                break ;
-            }
+        List<Integer> frequency = new ArrayList<>();
+        for(Integer i : hm.keySet()){
+            frequency.add(hm.get(i));
         }
         
-        return count ;
+        Collections.sort(frequency, Collections.reverseOrder());
+        for(int i: frequency){
+            fi++;
+            result += i;
+            if(result >= half) break;
+        }
+        
+        
+        return fi;
     }
 }
