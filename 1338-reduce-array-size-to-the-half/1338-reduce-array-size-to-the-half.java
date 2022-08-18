50b888ff1178e8c21ef4163a6edaad0828aaffc4
AKASHCHAURASIA2212 <102942376+AKASHCHAURASIA2212@users.noreply.github.com>
2022-08-18 19:15:18 +0530

    Time: 1802 ms (5.02%), Space: 134.1 MB (5.03%) - LeetHub

--- 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.java
+++ 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.java
@@ -1,5 +1,24 @@
 class Solution {
+    
+     public void Display(int[] arr)
+    {
+       for(int i=0;i<arr.length;i++)
+       {
+           System.out.print(arr[i]+ " ");
+       }
+        System.out.println();
+    }
+    
+       public void Display(List<Integer> arr)
+    {
+       for(int i=0;i<arr.size();i++)
+       {
+           System.out.print(arr.get(i)+ " ");
+       }
+           System.out.println();
+    }
     public int minSetSize(int[] arr) {
+        
         int half = arr.length / 2;
         int result = 0;
         int fi = 0;
@@ -11,8 +30,10 @@ class Solution {
         for(Integer i : hm.keySet()){
             frequency.add(hm.get(i));
         }
-        
+        // System.out.println(frequency);
+        Display(frequency);
         Collections.sort(frequency, Collections.reverseOrder());
+        Display(frequency);
         for(int i: frequency){
             fi++;
             result += i;
