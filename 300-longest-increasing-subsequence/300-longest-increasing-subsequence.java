dabcd68c295d23c9235404db38d9da616496a683
AKASHCHAURASIA2212 <102942376+AKASHCHAURASIA2212@users.noreply.github.com>
2022-08-25 00:19:44 +0530

    Time: 3 ms (98.67%), Space: 43.9 MB (82.24%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.java
@@ -1,28 +1,58 @@
 class Solution {
-    public int lengthOfLIS(int[] nums) {
-        int lis[] = new int[nums.length];
+//     public int lengthOfLIS(int[] nums) {
+//         int lis[] = new int[nums.length];
         
-        lis[0]=1;
+//         lis[0]=1;
+        
+//         for(int i=1;i<nums.length;i++)
+//         {
+//             int max = 0 ;
+            
+//             for(int j=0;j<i;j++)
+//             {
+//                 if(nums[j]<nums[i])
+//                 {
+//                     max = Math.max(max,lis[j]);
+//                 }
+//             }
+//             lis[i]=max+1 ;
+//         }
+        
+//         int len=0;
+//         for(int x:lis)
+//         {
+//             len=Math.max(len,x);
+//         }
+        
+//         return len ;
+//     }
+    
+     public int lengthOfLIS(int[] nums) {
+        int tails[] = new int[nums.length];
+        
+        tails[0]=nums[0];
+         int len=1;
         
         for(int i=1;i<nums.length;i++)
         {
-            int max = 0 ;
-            
-            for(int j=0;j<i;j++)
+             if(nums[i]>tails[len-1])
+             {
+                 len++;
+                 tails[len-1]=nums[i];
+             }
+            else
             {
-                if(nums[j]<nums[i])
+                int idx = Arrays.binarySearch(tails,0,len-1,nums[i]);
+                if(idx<0)
                 {
-                    max = Math.max(max,lis[j]);
+                    idx=-idx;
+                    idx=idx-1;
                 }
+                
+                tails[idx]=nums[i];
             }
-            lis[i]=max+1 ;
         }
-        
-        int len=0;
-        for(int x:lis)
-        {
-            len=Math.max(len,x);
-        }
+      
         
         return len ;
     }
