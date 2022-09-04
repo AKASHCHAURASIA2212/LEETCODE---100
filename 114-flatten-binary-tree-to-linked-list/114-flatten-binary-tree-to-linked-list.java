0d93a7989f8cddff67534a8c6502afd44a879c01
AKASHCHAURASIA2212 <102942376+AKASHCHAURASIA2212@users.noreply.github.com>
2022-09-04 14:38:44 +0530

    Time: 0 ms (100.00%), Space: 41.6 MB (97.93%) - LeetHub

--- 114-flatten-binary-tree-to-linked-list/114-flatten-binary-tree-to-linked-list.java
+++ 114-flatten-binary-tree-to-linked-list/114-flatten-binary-tree-to-linked-list.java
@@ -14,28 +14,41 @@
  * }
  */
 class Solution {
-          public void flatten(TreeNode root) {
-              
-        if (root == null) return;
-              
-        Stack<TreeNode> st = new Stack<TreeNode>();
-              
-        st.push(root);
-              
-        while (!st.isEmpty()){
-            
-            TreeNode curr = st.pop();
-            
-            if (curr.right!=null)  
-                 st.push(curr.right);
-            
-            if (curr.left!=null)  
-                 st.push(curr.left);
-            
-            if (!st.isEmpty()) 
-                 curr.right = st.peek();
-            
-            curr.left = null;  
-        }
-    }
+      TreeNode prev = null ;
+      public void flatten(TreeNode root) {
+          if(root==null){
+              return;
+          }
+          flatten(root.right);
+          flatten(root.left);
+          root.left = null;
+          root.right=prev;
+          prev=root;
+                  
+     }
 }
+//           public void flatten(TreeNode root) {
+              
+//         if (root == null) return;
+              
+//         Stack<TreeNode> st = new Stack<TreeNode>();
+              
+//         st.push(root);
+              
+//         while (!st.isEmpty()){
+            
+//             TreeNode curr = st.pop();
+            
+//             if (curr.right!=null)  
+//                  st.push(curr.right);
+            
+//             if (curr.left!=null)  
+//                  st.push(curr.left);
+            
+//             if (!st.isEmpty()) 
+//                  curr.right = st.peek();
+            
+//             curr.left = null;  
+//         }
+//     }
+// }
