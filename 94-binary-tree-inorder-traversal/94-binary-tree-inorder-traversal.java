a021e750fef2d0deb4d3cbd735331da1c41d740a
AKASHCHAURASIA2212 <102942376+AKASHCHAURASIA2212@users.noreply.github.com>
2022-09-08 06:21:04 +0530

    Time: 0 ms (100.00%), Space: 41.8 MB (80.45%) - LeetHub

--- 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.java
+++ 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.java
@@ -13,6 +13,18 @@
  *     }
  * }
  */
+
+// morris treversl
+ // steps 
+ //  1. if node.left == null
+ //      1.1 print node
+ //      1.2go to right
+ //  2.if node.left != null 
+ //      2.1 left is not processed
+ //      2.2 iop.right = node and go to left 
+ //      2.3 if left is procssed
+ //      2.4 iop.right = null ,print and go to right 
+
 class Solution {
     public List<Integer> inorderTraversal(TreeNode root) {
         List<Integer> val = new ArrayList<>();
