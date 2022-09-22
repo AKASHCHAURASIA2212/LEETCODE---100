d452aac935ab55603e31293415b7cfaf109600e1
AKASHCHAURASIA2212 <102942376+AKASHCHAURASIA2212@users.noreply.github.com>
2022-09-22 23:45:55 +0530

    Time: 14 ms (48.27%), Space: 52.1 MB (39.44%) - LeetHub

--- 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.java
+++ 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.java
@@ -1,17 +1,19 @@
 class Solution {
-    public String reverse(String str){
-        String rev="";
-        for(int i=0;i<str.length();i++){
-            rev=str.charAt(i)+rev;
-        }
-        return rev ;
+    public String revWords(String str){
+        StringBuilder sb = new StringBuilder(str);
+        sb = sb.reverse();
+        return sb.toString();
     }
     public String reverseWords(String s) {
         String[] dummy = s.split(" ");
-        String ans = "";
+        StringBuilder ans = new StringBuilder();
         for(int i=0;i<dummy.length;i++){
-            ans+=reverse(dummy[i])+" ";
+            ans.append(revWords(dummy[i]));
+            if(i!=dummy.length-1)
+                ans.append(" ");
         }
-        return ans.substring(0,ans.length()-1);
+        return ans.toString();
+        
+      
     }
 }
