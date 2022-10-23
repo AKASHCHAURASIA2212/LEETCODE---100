0bdfc34bae2ab698359861e0a7e65b792e68e748
AKASHCHAURASIA2212 <102942376+AKASHCHAURASIA2212@users.noreply.github.com>
2022-10-24 00:18:02 +0530

    Time: 11 ms (39.78%), Space: 43.8 MB (91.81%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.java
+++ 0645-set-mismatch/0645-set-mismatch.java
@@ -1,30 +1,24 @@
+
 class Solution {
     public int[] findErrorNums(int[] nums) {
-        // ArrayList<Integer> al = new ArrayList<>();
-        HashMap<Integer,Integer> hm = new HashMap<>();
-        for(int i=0;i<nums.length;i++){
-            if(hm.containsKey(nums[i])==false){
-                hm.put(nums[i],1);
-            }else{
-                int val = hm.get(nums[i]);
-                hm.put(nums[i],val+1);
+        
+        int dup = 0, miss = 0;
+        HashSet<Integer> set = new HashSet<>();
+        
+        // find duplicate using set
+        for(int i = 0; i < nums.length; ++i){
+            if(set.contains(nums[i]))   dup = nums[i];
+            set.add(nums[i]);
+        }
+        
+        // find missing
+        for(int i = 1; i <= nums.length; ++i){
+            if(set.contains(i) == false){
+                miss = i;
+                break;
             }
         }
-        System.out.println(hm);
-        int key = 0 , value = 0 ;
-        for(int i=1;i<=nums.length;i++){
-            if(hm.containsKey(i)==false){
-                key = i;
-            }
-            
-            if(hm.get(i)!=null && hm.get(i)==2){
-                value=i;
-            }
-        }
-        // int[] myans = new int[al.size()];
-        int myans[] = new int[2];
-        myans[0]=value;
-        myans[1]=key;
-        return myans ;
+        
+        return new int[]{dup, miss};
     }
 }
