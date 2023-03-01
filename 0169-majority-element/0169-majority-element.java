c88b5eb5a6bdfa1f782c53fbb6b76a1291f290fe
AkashChaurasia22 <102942376+AKASHCHAURASIA2212@users.noreply.github.com>
2023-03-01 10:14:18 +0530

    Time: 14 ms (21.20%), Space: 48.3 MB (18.24%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.java
@@ -0,0 +1,28 @@
+class Solution {
+    
+    public int solve(int[] arr){
+        HashMap<Integer, Integer> hm = new HashMap<>();
+
+        for (int x : arr) {
+            if (hm.containsKey(x)) {
+                hm.put(x, hm.get(x) + 1);
+            } else {
+                hm.put(x, 1);
+            }
+        }
+
+        for (int elem : arr) {
+            if (hm.get(elem) > arr.length / 2) {
+                // System.out.println("Majority element --> " + elem);
+                return elem;
+        
+            }
+        }
+        
+        return -1;
+
+    }
+    public int majorityElement(int[] nums) {
+        return solve(nums);
+    }
+}
